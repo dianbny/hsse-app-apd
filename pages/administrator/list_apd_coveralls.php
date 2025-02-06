fdb6af3a73f2c59bf222c0a27fa60302891c4048
dianbny <pratama.dianbny@gmail.com>
2025-02-07 06:58:22 +0800

    Merubah tabel dan pengambilan data untuk tabel

--- pages/administrator/list_apd_coveralls.php
+++ pages/administrator/list_apd_coveralls.php
@@ -2,8 +2,6 @@
     if(!isset($_SESSION['status-login']) || $_SESSION['level'] != "administrator"){ 
         header('location:'.BASEURL.'/logout');
     }
-
-    $_SESSION['tab-content'] = "1";
 ?>
 <div class="title-page">
     <img src="<?= BASEURL; ?>/assets/img/coveralls.png" alt="">Coveralls
@@ -20,10 +18,10 @@
     <button type="button" id="search" class="button-search">Search</button>
 
     <div class="top">
-        <h5>List APD HSSE Bunyu Field</h5>
+        <h5>List Coveralls</h5>
         <div>
-            <a href="<?= BASEURL; ?>/add-apd" class="link-crud" style="background-color:#008000;"><i class="fa fa-plus" aria-hidden="true"></i></a>
-            <a href="" class="link-crud" style="background-color:#FF4500;"><i class="fa fa-refresh" aria-hidden="true"></i></a>
+            <a href="<?= BASEURL; ?>/add-coveralls" class="link-crud" style="background-color:#008000;"><i class="fa fa-plus" aria-hidden="true"></i></a>
+            <a href="<?= BASEURL; ?>/list-coveralls" class="link-crud" style="background-color:#FF4500;"><i class="fa fa-refresh" aria-hidden="true"></i></a>
            
         </div>
     </div>
@@ -32,41 +30,31 @@
             <thead>
                 <tr>
                     <th>#</th>
-                    <th>Code APD</th>
                     <th>Name</th>
-                    <th>Category</th>
-                    <th>Location APD</th>
-                    <th>Detail Rack Num.</th>
+                    <th>Merk</th>
+                    <th>Type</th>
+                    <th>Size</th>
                     <th>Stock</th>
-                    <th>Expired Date</th>
+                    <th>Created At</th>
+                    <th>Updated At</th>
                     <th style="text-align:center;">Action</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
-                    if($cekData->cekDataAll('_tb_item_apd') > 0){
+                    if($cekData->cekDataAll('_tb_coveralls') > 0){
                         $no = 1; 
-                        foreach($getData->getDataAll('_tb_item_apd','_id') as $row){ ?>
+                        foreach($getData->getDataAll('_tb_coveralls','_id') as $row){ ?>
                             <tr>
                                 <td><?= $no++.'.'; ?></td>
-                                <td><?= $row['_kode_apd']; ?></td>
-                                <td><?= $row['_nama_apd']; ?></td>
-                                <td>
-                                    <?php
-                                        $kategori = $getData->getDataFr('_tb_category_apd', '_id', $row['_category']);
-                                        echo $kategori['_category'];
-                                    ?>
-                                </td>
-                                <td><?= $row['_lokasi_apd']; ?></td>
-                                <td>
-                                    <?php
-                                        $rak = $getData->getDataFr('_tb_rack', '_id', $row['_rack']);
-                                        echo $rak['_code_rack'].'.'.$row['_detail_rack_num'];
-                                    ?>
-                                </td>
-                                <td><?= $row['_stok_apd']; ?></td>
-                                <td><?= $row['_tgl_expired']; ?></td>
-                                <td style="text-align:center;"><a href="<?= BASEURL; ?>/detail-apd/<?= $row['_id']; ?>" class="link-edit-detail" style="background-color:#FF4500;"><i class="fa fa-info-circle" aria-hidden="true"></i></a></td>
+                                <td><?= $row['_nama']; ?></td>
+                                <td><?= $row['_merk']; ?></td>
+                                <td><?= $row['_jenis']; ?></td>
+                                <td><?= $row['_ukuran']; ?></td>
+                                <td><?= $row['_stok']; ?></td>
+                                <td><?= $row['_created_at']; ?></td>
+                                <td><?= $row['_updated_at']; ?></td>
+                                <td style="text-align:center;"><a href="<?= BASEURL; ?>/detail-coveralls/<?= $row['_id']; ?>" class="link-edit-detail" style="background-color:#FF4500;"><i class="fa fa-info-circle" aria-hidden="true"></i></a></td>
                         
                             </tr>
                   <?php }
