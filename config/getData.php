f4090260e582df37e8cee162ac649bed2b848e86
dianbny <pratama.dianbny@gmail.com>
2025-02-07 06:54:37 +0800

    Menambah clause deleted null dan sum data tabel

--- config/getData.php
+++ config/getData.php
@@ -5,7 +5,7 @@
 
 		//Get Data 1 Clause
 		function getDataFr($tabel, $colfr, $datafr){
-			$data = mysqli_query($this->koneksi,"SELECT * FROM $tabel WHERE $colfr = '$datafr'");
+			$data = mysqli_query($this->koneksi,"SELECT * FROM $tabel WHERE $colfr = '$datafr' AND _deleted_at IS NULL");
 			$getData = mysqli_fetch_assoc($data);
 			
 			return $getData;
@@ -13,12 +13,20 @@
 
         //Get Data All 0 Clause
         function getDataAll($table, $col){
-			$dataAll = mysqli_query($this->koneksi,"SELECT * FROM $table ORDER BY $col ASC");
+			$dataAll = mysqli_query($this->koneksi,"SELECT * FROM $table WHERE _deleted_at IS NULL ORDER BY $col ASC");
 			while($listDataAll = mysqli_fetch_assoc($dataAll)){
 				$result[] = $listDataAll;
 			}
 
 			return $result;
+		}
+
+		//Get Sum Table
+		function getSumData($table){
+			$sumData = mysqli_query($this->koneksi,"SELECT SUM(_stok) AS stok FROM $table WHERE _deleted_at IS NULL");
+			$getData = mysqli_fetch_assoc($sumData);
+			
+			return $getData;
 		}
 
 
