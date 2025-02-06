5146743d4558f5be85bd1a2f1dcaec6160b8b46d
dianbny <pratama.dianbny@gmail.com>
2025-02-07 06:55:18 +0800

    Merubah save data apd menjadi save data coveralls

--- config/saveData.php
+++ config/saveData.php
@@ -9,8 +9,8 @@
         }
 
         //Save Data New APD
-        public function saveDataAPD($code, $name, $category, $location, $rack, $rackNum, $stock, $expired, $photo, $createdAt, $updatedAt){
-            mysqli_query($this->koneksi,"INSERT INTO _tb_item_apd (_kode_apd, _nama_apd, _category, _lokasi_apd, _rack, _detail_rack_num, _stok_apd, _tgl_expired, _foto_apd, _created_at, _updated_at) VALUES ('$code','$name','$category','$location','$rack','$rackNum','$stock','$expired','$photo','$createdAt', '$updatedAt')");
+        public function saveDataCoveralls($name, $merk, $type, $size, $stock, $location, $rack, $rackNum, $photo, $createdAt, $updatedAt){
+            mysqli_query($this->koneksi,"INSERT INTO _tb_coveralls (_nama, _merk, _jenis, _ukuran, _stok, _lokasi_apd, _rack, _detail_rack_num, _foto, _created_at, _updated_at) VALUES ('$name','$merk','$type','$size','$stock','$location','$rack','$rackNum','$photo','$createdAt', '$updatedAt')");
         }
 
 
